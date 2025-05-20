b1fdbea2cb9ac2c146d53c7c708df878b61580bf
Marcelo Wolfgang <grillo@gmail.com>
2025-05-20 20:27:02 -0300

    06 - login

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './app'
+import App from './App'
 import Admin from './pages/admin'
 import './index.css'
 
