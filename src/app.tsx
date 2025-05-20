2d239a00dc5f9fa3d884635c6030476d94bc8ff7
Marcelo Wolfgang <grillo@gmail.com>
2025-05-20 16:32:22 -0300

    09 - vercel

--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">com.grillo.apt 2</h1>
-    </div>
-  )
-}
-
-export default App
