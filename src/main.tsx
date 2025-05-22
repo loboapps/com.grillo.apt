ccc72f2bb282183bb0f1003997c6bfdf2b60cdd7
Marcelo Wolfgang <grillo@gmail.com>
2025-05-22 15:08:35 -0300

    02 - nav + font + nomenclatura

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Admin from './pages/admin'
-import ConfigEtapa from './pages/ConfigEtapa'
-import GerenciarEtapa from './pages/GerenciarEtapa'
-import Mesas from './pages/Mesas'
 import EtapaJogadores from './pages/etapa_jogadores'
 import EtapaEliminacaoRebuy from './pages/etapa_eliminacao_rebuy'
 import './index.css'
@@ -18,11 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/admin" element={<Admin />} />
-          <Route path="/config-etapa" element={<ConfigEtapa />} />
-          <Route path="/gerenciar-etapa" element={<GerenciarEtapa />} />
-          <Route path="/mesas" element={<Mesas />} />
           <Route path="/config-etapa/jogadores" element={<EtapaJogadores />} />
           <Route path="/gerenciar-etapa/eliminacao" element={<EtapaEliminacaoRebuy />} />
+          <Route path="/config-etapa/mesas" element={<Mesas />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
