ed6eb052b07a5e3d51438ad34b0db53eb4933f0e
Marcelo Wolfgang <grillo@gmail.com>
2025-06-02 16:08:51 -0300

    05 - nav gerencimento // mesas

--- src/pages/mesas.tsx
+++ src/pages/mesas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from '../components/Nav'
 import { useLocation } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
